38f526e94f9562c408e5c556bd2f12647e050116
SYdulu <tsydulu9@gmail.com>
2019-12-16 15:11:08 +0530

    created print

---
+++ GitHubSohailDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitHubSohailDemo
+//
+//  Created by Sohail Binanzala on 16/12/19.
+//  Copyright © 2019 Sohail Binanzala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+       print("..........GIT HUB DEMO...............")
+              print("..........GIT HUB DEMO...............")
+              print("..........GIT HUB DEMO...............")
+        print("pora jaffa")
+        print("sohail jaffa")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
